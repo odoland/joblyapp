63ed08919680bd3b40f520bff36f6e496c4c957f
odoland <orlando.wong@yahoo.com>
2019-05-22 19:36:48 -0700

    styling navbar

--- jobly/src/NavBarComponents/Navbar.js
+++ jobly/src/NavBarComponents/Navbar.js
@@ -1,34 +1,46 @@
 import React, { Component } from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 const style = {
   color: 'skyblue'
 }
+
 class Navbar extends Component {
 
-
   render() {
-    console.log("Rendering:", this.props.isLoggedIn, "logged in?");
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink className="navbar-brand" exact to='/' activeStyle={style}>Jobly</NavLink>
-        <NavLink className="navbar-brand" to='/companies' activeStyle={style}>Companies</NavLink>
-        <NavLink className="navbar-brand" to='/jobs' activeStyle={style}>Jobs</NavLink>
-        <NavLink className="navbar-brand" to='/profile' activeStyle={style}>Profile</NavLink>
-        {this.props.isLoggedIn ? <button
-          className="navbar-brand"
-          onClick={this.props.logout}>
-          Log Out
-        </button> :
-        
-        <NavLink
-          className="navbar-brand"
-          to='/login' 
-          activeStyle={style}>
-          Log In
-        </NavLink>}
-      </nav>
 
+    const LogoutButton = (
+      <button
+        id="Navbar-Logout"
+        className="navbar-brand"
+        onClick={this.props.logout}>
+        Log Out
+      </button>
+    );
+
+    const LoginNavLink = (
+      <NavLink
+        className="navbar-brand"
+        to='/login' activeStyle={style}>
+        Log In
+      </NavLink>
+    );
+
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="mr-5 order-0">
+          <NavLink className="navbar-brand" exact to='/' activeStyle={style}>Jobly</NavLink>
+        </div>
+        <div class="order-1 mx-auto">
+          <NavLink className="navbar-brand" exact to='/companies' activeStyle={style}>Companies</NavLink>
+          <NavLink className="navbar-brand" exact to='/jobs' activeStyle={style}>Jobs</NavLink>
+          <NavLink className="navbar-brand" exact to='/profile' activeStyle={style}>Profile</NavLink>
+        </div>
+        <div class="order-3 ml-5">
+          {this.props.isLoggedIn ? LogoutButton : LoginNavLink}
+        </div>
+      </nav>
     )
   }
 
